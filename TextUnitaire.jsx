2568546b58ea5c268177c6c9dcf3838e9e52c8b4
DONMEZACAROLE <109277798+DONMEZACAROLE@users.noreply.github.com>
2023-11-13 17:07:36 +0100

    TextUnitaire
    .jsx

---
+++ TextUnitaire.jsx
@@ -0,0 +1,21 @@
+{/*import React from 'react'
+import CreateBooks from './CreateBooks';
+
+const TextUnitaire=()=> {
+  return (
+      <div>
+          <p>le composant 'CreateBooks' est utilisé pour afficher les détails d'un livre dans l'application de gestion de la bibliothèque.<br />Propriétés
+              Le composant 'CreateBook'attend les propriétés suivantes:
+              -titre(chaine de caractères):le titre du livre.<br />
+              -author(chaine de caractères):le nom de l'auteur du livre.<br />
+              -description(chaine de caractères):une description du contenu du livre.<br />
+          </p>
+          <CreateBooks titre='le petit prince'
+              auteur='Antoine de Saint-Exupéry'
+              description="le petit prince est une oeuvre de langue francaise ,plus connue d'Antoine Saint-Exupéry" />
+          Rendu: le composant va rendreles informations du livre de la manière suivante
+          [Rendu du composant CreateBooks]
+    </div>
+  )
+}
+export default TextUnitaire;*/}
